740d0eb19e1bf8c91f86641c7e24151c5d3d9ec3
donnyesq <adonis@darmas.ca>
2020-08-24 21:50:39 -0400

    exercise 1 complete

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { bindActionCreators } from "redux";
+
+const initialState = {};
+
+export default function cartReducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
