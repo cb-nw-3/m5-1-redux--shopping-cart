56845b238a90b7cf26b53db51b408ee481fe1826
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-08-24 22:05:21 -0400

    ex2 started, missing item title and quantity w/ X

---
+++ src/components/Cart.js
@@ -0,0 +1,71 @@
+import React from "react";
+import styled from "styled-components";
+import Button from "./Button";
+import CartItem from "./CartItem";
+
+const Cart = () => {
+  return (
+    <Wrapper>
+      <CartHeader>
+        <CartTitle>Your Cart</CartTitle>
+        <CartItemNumber> 0 Items</CartItemNumber>
+        <CartList>
+          <CartItem />
+        </CartList>
+      </CartHeader>
+      <CartCheckout>
+        <CartTotal>
+          Total:<Total>$0.00</Total>
+        </CartTotal>
+        <Button>Purchase</Button>
+      </CartCheckout>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  position: sticky;
+  top: 0;
+  color: white;
+  display: flex;
+  min-width: 300px;
+  height: 100vh;
+  padding: 15px 0;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: rgb(64, 31, 67);
+`;
+
+const CartHeader = styled.div`
+  padding: 0 25px;
+`;
+const CartTitle = styled.h2`
+  font-weight: bold;
+  margin-bottom: 0px;
+`;
+const CartItemNumber = styled.p`
+  font-weight: normal;
+  opacity: 0.7;
+  margin-top: 8px;
+  margin-bottom: 0px;
+`;
+const CartCheckout = styled.div`
+  display: flex;
+  padding: 0 25px 50px 25px;
+`;
+const CartTotal = styled.div`
+  color: white;
+  font-size: 22px;
+`;
+const Total = styled.span`
+  font-weight: bold;
+  font-size: 22px;
+`;
+const CartList = styled.ul`
+  list-style-type: none;
+  display: flex;
+  padding-top: 25px;
+  flex-direction: column;
+`;
+
+export default Cart;
---
+++ src/components/CartItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const CartItem = () => {
+  return <Wrapper></Wrapper>;
+};
+
+const Wrapper = styled.div`
+  border: 2px dashed rgba(250, 250, 250, 0.2);
+  padding: 10px;
+`;
+export default CartItem;
