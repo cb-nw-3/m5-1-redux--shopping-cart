fb4cf609c1a9ca043b6129bebbd1444fa24231ce
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-08-25 22:17:28 -0400

    ex3a added ADD_ITEM action to redux and storeItem

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,16 @@ const initialState = {};
 
 export default function cartReducer(state = initialState, action) {
   switch (action.type) {
+    case "ADD_ITEM": {
+      console.log(`${action.item.title} was added to the cart!`);
+      return {
+        ...state,
+        [action.item.id]: {
+          ...action.item,
+          quantity: 1,
+        },
+      };
+    }
     default:
       return state;
   }
