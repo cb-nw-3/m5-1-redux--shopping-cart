5b0bd47c0589f75917b403c8b13541c1e35397d9
donnyesq <adonis@darmas.ca>
2020-08-26 21:50:50 -0400

    exercise 3A complete

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,17 @@
+import produce from "immer";
+
 const initialState = {};
 
 export default function cartReducer(state = initialState, action) {
   switch (action.type) {
+    case "ADD_ITEM":
+      console.log("state", state);
+      return produce(state, (draftState) => {
+        draftState[action.item.id] = action.item;
+        !draftState[action.item.id].quantity
+          ? (draftState[action.item.id].quantity = 1)
+          : draftState[action.item.id].quantity++;
+      });
     default:
       return state;
   }
