e70656cb193a2b3bf569fe83974b9633a7ed178e
carldumas <dumas.carl@gmail.com>
2020-09-08 16:58:52 -0400

    M5-1 Redux: Exercise 1 - Install dependencies using NPM + format clean up

--- src/components/App.js
+++ src/components/App.js
@@ -6,38 +6,38 @@ import ItemGrid from './ItemGrid';
 import GlobalStyles from './GlobalStyles';
 
 const App = () => {
-  return (
-    <Wrapper>
-      <Header>
-        <Logo />
-      </Header>
-      <ItemGridWrapper>
-        <ItemGrid />
-      </ItemGridWrapper>
+    return (
+        <Wrapper>
+            <Header>
+                <Logo />
+            </Header>
+            <ItemGridWrapper>
+                <ItemGrid />
+            </ItemGridWrapper>
 
-      <GlobalStyles />
-    </Wrapper>
-  );
+            <GlobalStyles />
+        </Wrapper>
+    );
 };
 
 const Wrapper = styled.div`
-  position: relative;
+    position: relative;
 `;
 
 const Header = styled.header`
-  grid-area: header;
-  padding: 32px 64px;
+    grid-area: header;
+    padding: 32px 64px;
 `;
 
 const ItemGridWrapper = styled.main`
-  grid-area: main;
-  padding: 16px 64px;
+    grid-area: main;
+    padding: 16px 64px;
 `;
 
 const CartWrapper = styled.div`
-  grid-area: sidebar;
-  border-left: 3px dashed #ff406e;
-  padding-left: 8px;
+    grid-area: sidebar;
+    border-left: 3px dashed #ff406e;
+    padding-left: 8px;
 `;
 
 export default App;
--- src/components/ItemGrid.js
+++ src/components/ItemGrid.js
@@ -6,19 +6,19 @@ import { STORE_ITEMS } from '../data';
 import StoreItem from './StoreItem';
 
 const ItemGrid = () => {
-  return (
-    <Wrapper>
-      {STORE_ITEMS.map((item) => (
-        <StoreItem key={item.id} {...item} />
-      ))}
-    </Wrapper>
-  );
+    return (
+        <Wrapper>
+            {STORE_ITEMS.map((item) => (
+                <StoreItem key={item.id} {...item} />
+            ))}
+        </Wrapper>
+    );
 };
 
 const Wrapper = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(220px, 1fr));
-  grid-gap: 24px;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(220px, 1fr));
+    grid-gap: 24px;
 `;
 
 export default ItemGrid;
